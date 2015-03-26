cf81935fdb6916d9e573e69a523143564eff8dfe
Mark Gandolfo <mark@gandolfo.com.au>
2015-03-26 15:52:54 +1000

    add more nos

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  nos = ["Nope", "No", "Not Yet"]
+  nos = ["Nope", "No", "Not Yet", "Maybe Tomorrow", "No Indeed", "Absolutely Not", "Under no circumstances", "Negative", "Not Today"]
   setTimeout(function() {
     console.log('a');
     document.getElementById('answer').innerHTML = nos[Math.floor(Math.random()*nos.length)];
